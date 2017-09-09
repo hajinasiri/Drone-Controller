d114143f7d0ab7e32c93d59e6ac2b595bc9eee08
hajinasiri <hajinasiri@gmail.com>
2017-09-09 00:38:39 +0000

    little more heroku preparation

--- production-server.js
+++ production-server.js
@@ -3,10 +3,10 @@ const express = require('express');
 
 const app = express();
 const indexPath = path.join(__dirname, './index.html');
-const publicPath = express.static(path.join(__dirname, './dist'));
+const publicPath = path.join(__dirname, './dist');
 
 app.set('port', process.env.PORT || 8080);
-app.use('/dist', publicPath);
+app.use('/dist', express.static(publicPath));
 app.get('/', (req, res) => res.sendFile(indexPath));
 
 app.listen(app.get('port'), () => {
