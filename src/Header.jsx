64ca227b95d7337e762911a2120d187b9379a80a
Hannah Kim <h.heeeun.kim@gmail.com>
2017-09-13 00:26:46 +0000

    finished basic layout

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+    <div>
+      <div className="header">
+        <img src="../img/simdrone_logo.png" id= "logo" />
+      </div>
+    </div>
+    )
+  }
+}
+export default Header;
