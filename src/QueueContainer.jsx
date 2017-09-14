97163cb09f67c58c5ffaf8332ab0b0b809d94517
Hannah Kim <h.heeeun.kim@gmail.com>
2017-09-14 01:01:03 +0000

    finished basic countdown time queue

--- src/QueueContainer.jsx
+++ src/QueueContainer.jsx
@@ -23,12 +23,12 @@ class QueueContainer extends Component {
     let timeLeftVar = this.secondsToTime(this.state.seconds);
     this.setState({ time: timeLeftVar });
   }
-
   startTimer() {
     if (this.timer == 0) {
       this.timer = setInterval(this.countDown, 1000);
     }
   }
+
 
   countDown() {
     let seconds = this.state.seconds - 1;
