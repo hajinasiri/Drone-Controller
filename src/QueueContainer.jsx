e7fa6240244289b8abce710635e221c0018b4064
Hannah Kim <h.heeeun.kim@gmail.com>
2017-09-14 00:50:44 +0000

    basic countdown feature implemented

--- src/QueueContainer.jsx
+++ src/QueueContainer.jsx
@@ -1,6 +1,47 @@
 import React, {Component} from 'react';
 
+
 class QueueContainer extends Component {
+  constructor() {
+    super();
+    this.state = { time: {}, seconds: 60 };
+    this.timer = 0;
+    this.startTimer = this.startTimer.bind(this);
+    this.countDown = this.countDown.bind(this);
+  }
+
+  secondsToTime(secs){
+    let seconds = secs;
+
+    let obj = {
+      "s": seconds
+    };
+    return obj;
+  }
+
+  componentDidMount() {
+    let timeLeftVar = this.secondsToTime(this.state.seconds);
+    this.setState({ time: timeLeftVar });
+  }
+
+  startTimer() {
+    if (this.timer == 0) {
+      this.timer = setInterval(this.countDown, 1000);
+    }
+  }
+
+  countDown() {
+    let seconds = this.state.seconds - 1;
+    this.setState({
+      time: this.secondsToTime(seconds),
+      seconds: seconds
+    });
+
+    if (seconds == 0) {
+      clearInterval(this.timer);
+    }
+  }
+
   render() {
     return (
     <div>
@@ -13,7 +54,8 @@ class QueueContainer extends Component {
           <div className="users-item active">
             <div className="queue-current">
               <span className="users-name">ESHA</span>
-              <span>0:30</span>
+              <button onClick={this.startTimer}>Start</button>
+              <span> {this.state.time.s} seconds remaining</span>
             </div>
           </div>
           <div className="users-item">
